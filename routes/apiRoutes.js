3ec0a8a4ed0f2430c38404c7145baf8b367f6737
Zarin Salim <zarin_96@hotmail.co.uk>
2021-04-24 12:09:16 +0100

    updating routes with post and put

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,16 +3,30 @@ const db = require("../models");
 
 router.get("/workouts", (req, res) => {
     db.Workout.find()
-    .then(workouts => res.json(workouts))
-    .catch(err => res.json(err));
+        .then(workouts => res.json(workouts))
+        .catch(err => res.json(err));
 });
 
 router.post("/workouts", (req, res) => {
     Workout.create({})
-    .then (dbWorkout => {
-        res.jsob(dbWorkout);
-    })
-    .catch(err => {
-        res.status(400).json(err);
-    });
+        .then(dbWorkout => {
+            res.jsob(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put("/workouts/:id", (req, res) => {
+    Workout.updateOne(
+        { _id: req.params.id },
+        { $push: { exercises: req.body } },
+        { new: true }
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
