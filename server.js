a1430659e23b144a6e3c04b821c61e0b42e35dfd
Zarin Salim <zarin_96@hotmail.co.uk>
2021-04-25 13:47:27 +0100

    updated server to work with atlas

--- server.js
+++ server.js
@@ -8,15 +8,21 @@ const app = express();
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 app.use(require("./routes/apiRoutes"));
 app.use(require("./routes/htmlRoutes"));
