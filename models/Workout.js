210bd471df8776b5f11e262e2a3f2256582aa1e2
Zarin Salim <zarin_96@hotmail.co.uk>
2021-04-25 13:08:58 +0100

    added to model to make total duration work

--- models/Workout.js
+++ models/Workout.js
@@ -33,6 +33,12 @@ const workoutSchema = new Schema({
     }]
 }, {toJSON: { virtuals: true }});
 
+workoutSchema.virtual("totalDuration").get(function() {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration
+    }, 0);
+})
+
 const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
